a95828c66115423665f9f8086fdee46ee26838a3
mohit60000 <mohit60000@ufl.edu>
2018-02-03 20:20:29 -0500

    Just making post endpoint

--- apiRoutes/routes.js
+++ apiRoutes/routes.js
@@ -2,8 +2,10 @@ module.exports = function(app) {
     var todoList = require('../controllers/apiEndpoints');
   
     // todoList Routes
-    app.route('/vision')
-      .get(todoList.googleVision);
+    //app.route('/vision')
+    //  .get(todoList.googleVision);
+
+    app.post("/", todoList.incomingRequest);
 
     
 };
